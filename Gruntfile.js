bd2ee148db5fb4610cc25b2c011a224ab6f31f24
Carsten Sandtner <sandtner@mediaman.de>
2014-12-09 15:43:39 +0100

    added my C64 Theme :)

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,7 @@ module.exports = function(grunt) {
 					'css/theme/sky.css': 'css/theme/source/sky.scss',
 					'css/theme/moon.css': 'css/theme/source/moon.scss',
 					'css/theme/solarized.css': 'css/theme/source/solarized.scss',
+					'css/theme/c64.css': 'css/theme/source/c64.scss',
 					'css/theme/blood.css': 'css/theme/source/blood.scss'
 				}
 			}
